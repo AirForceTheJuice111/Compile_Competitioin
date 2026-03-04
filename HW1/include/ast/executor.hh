f5e1eb3e47380ebadb62a9a1bd957e7e86c05791
dywsy21 <dywsy21@163.com>
2026-03-04 10:28:57 +0800

    HW1: executor prints at return; add .clang-format with 4-space indent + root Makefile format target

--- HW1/include/ast/executor.hh
+++ HW1/include/ast/executor.hh
@@ -9,26 +9,25 @@
 using namespace std;
 using namespace fdmj;
 
-// 执行程序并返回最终 return 语句的值
 int execute(Program *root);
 
-// 解释执行 Visitor：逐语句执行，维护变量表，返回 return 表达式的值
+// interpreter visitor：逐语句执行，维护变量表，返回 return 表达式的值
 class Executor : public ASTVisitor {
-public:
-  int result = 0;                  // 当前表达式的求值结果
-  map<string, int> varTable;       // 变量名 → 当前值
-  map<string, bool> varDefined;    // 变量名 → 是否已定义
+  public:
+    int result = 0; // 当前表达式的求值结果
+    map<string, int> varTable;
+    map<string, bool> varDefined;
 
-  Executor() : result(0) {}
-  void visit(Program *node) override;
-  void visit(MainMethod *node) override;
-  void visit(Assign *node) override;
-  void visit(Return *node) override;
-  void visit(BinaryOp *node) override;
-  void visit(UnaryOp *node) override;
-  void visit(IdExp *node) override;
-  void visit(OpExp *node) override;
-  void visit(IntExp *node) override;
+    Executor() : result(0) {}
+    void visit(Program *node) override;
+    void visit(MainMethod *node) override;
+    void visit(Assign *node) override;
+    void visit(Return *node) override;
+    void visit(BinaryOp *node) override;
+    void visit(UnaryOp *node) override;
+    void visit(IdExp *node) override;
+    void visit(OpExp *node) override;
+    void visit(IntExp *node) override;
 };
 
 #endif
