8cd50831d145f350a2952b8247e1d790a6d8e4c6
dywsy21 <dywsy21@163.com>
2026-03-12 10:30:56 +0800

    make format: hw2

--- HW2/lib/ast/semantanlyzer.cc
+++ HW2/lib/ast/semantanlyzer.cc
@@ -1,30 +1,30 @@
 #define DEBUG
 #undef DEBUG
 
+#include "namemaps.hh"
+#include "semant.hh"
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <vector>
-#include <algorithm>
-#include "namemaps.hh"
-#include "semant.hh"
 
 using namespace std;
 using namespace fdmj;
 
 // TODO
 
-AST_Semant_Map* semant_analyze(Program* node) {
-    std::cout << "TODO" << std::endl;
-    return nullptr;
-    // std::cerr << "Start Semantic Analysis" << std::endl;
-    // if (node == nullptr) {
-    //     return nullptr;
-    // }
-    // Name_Maps* name_maps = makeNameMaps(node);
-    // AST_Semant_Visitor semant_visitor(name_maps);
-    // semant_visitor.visit(node);
-    // std::cerr << "Semantic Analysis Done" << std::endl;
-    // return semant_visitor.getSemantMap();
+AST_Semant_Map *semant_analyze(Program *node) {
+  std::cout << "TODO" << std::endl;
+  return nullptr;
+  // std::cerr << "Start Semantic Analysis" << std::endl;
+  // if (node == nullptr) {
+  //     return nullptr;
+  // }
+  // Name_Maps* name_maps = makeNameMaps(node);
+  // AST_Semant_Visitor semant_visitor(name_maps);
+  // semant_visitor.visit(node);
+  // std::cerr << "Semantic Analysis Done" << std::endl;
+  // return semant_visitor.getSemantMap();
 }
 
 // void AST_Semant_Visitor::visit(Program* node) {
